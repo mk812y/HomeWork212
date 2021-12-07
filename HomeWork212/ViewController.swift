b0041c5b32f09008374ec4b65787441117260ae4
mk812y <mk812y@gmail.com>
2021-12-08 00:52:01 +0300

    Setup cornerRadius?

--- HomeWork212/ViewController.swift
+++ HomeWork212/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewRed: UIView!
+    @IBOutlet weak var viewYellow: UIView!
+    @IBOutlet weak var viewGreen: UIView!
+    @IBOutlet weak var viewButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let buttonFrameWidth = viewRed.layer.frame.width
+        viewRed.layer.cornerRadius = buttonFrameWidth / 2
+//        viewRed.clipsToBounds = true
+        viewYellow.layer.cornerRadius = buttonFrameWidth / 2
+        viewGreen.layer.cornerRadius = buttonFrameWidth / 2
     }
 
-
+    @IBAction func buttonAction(_ sender: UIButton) {
+    }
+    
 }
 
